517253b8dc7c4b96f4c184bf332d3c7c97ae7a65
ZANJAY-KS <zanjay.pvt@gmail.com>
2025-03-31 22:01:10 +0530

    first commit

--- tournaments/views.py
+++ tournaments/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Tournament
+
+def tournament_list(request):
+    tournaments = Tournament.objects.all()
+    return render(request, 'tournaments/tournaments.html', {'tournaments': tournaments})
